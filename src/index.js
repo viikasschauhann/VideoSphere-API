6af8bfcf03cc8f75f36d2ac9f0d1cfe21de0ce3f
viikasschauhann <vchauhan6155@gmail.com>
2025-10-31 23:32:27 +0530

    adding new packages for creating instance.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
-
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import {app} from "./app.js";
 
 dotenv.config({
     path: './env'
 })
 
-connectDB();
+connectDB()
+.then(() => {
+    app.listen( process.env.PORT || 8000, () => {
+        console.log(`Server is listening on port: ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log('mongodb connection failed', err);
+    
+})
