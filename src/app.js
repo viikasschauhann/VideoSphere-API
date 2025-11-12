fd085c8d03c8b4073f835c488f5b1d988f0af1ae
viikasschauhann <vchauhan6155@gmail.com>
2025-11-12 23:47:20 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -14,6 +14,13 @@ app.use(express.urlencoded({extended: true, limit: "16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser());
 
+
+// import routes
+import userRouter from "./routes/user.routes.js"
+
+// routes declaration
+app.use("/api/v1/users", userRouter);
+
 app.get('/', (req, res) => {
    res.send('hii, i am server')
     
