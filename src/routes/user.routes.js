b1fe06aee928a4e579d8c34137378d7128194a43
viikasschauhann <vchauhan6155@gmail.com>
2026-01-31 19:57:33 +0530

    refactor: update response format for current user retrieval and reorder route definitions

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -22,8 +22,8 @@ router.route("/register").post(
 router.route("/login").post(loginUser);
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken);
-router.route("/current-user").get(verifyJWT, getCurrentUser);
 router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router.route("/current-user").get(verifyJWT, getCurrentUser);
 router.route("/update-account").patch(verifyJWT, updateAccountDetails);
 router.route("/update-avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
 router.route("/update-coverImage").patch(verifyJWT, upload.single("coverImage"), updateUserCoverImage);
