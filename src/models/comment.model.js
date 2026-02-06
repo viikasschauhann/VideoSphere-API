9979c4cc72fbb334118f31a1e7f9d48c6ce7ea50
viikasschauhann <vchauhan6155@gmail.com>
2026-02-06 19:45:32 +0530

    feat: add models for comments, likes, playlists, and tweets; update subscription model comments

--- src/models/comment.model.js
+++ src/models/comment.model.js
@@ -1,4 +1,5 @@
 import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const commentSchema = new Schema({
     content: {
@@ -15,4 +16,5 @@ const commentSchema = new Schema({
     }
 }, {timestamps: true})
 
+commentSchema.plugin(mongooseAggregatePaginate);
 export const Comment = mongoose.model("Comment", commentSchema);
